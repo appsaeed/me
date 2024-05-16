854533d3094d5221705e5d873f0509b7b2ea931a
Saeed Hossen <appsaeed7@gmail.com>
2024-05-16 15:24:41 +0600

    remove extract plugin file and update workflows

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,26 @@
 import { defineConfig } from "vite";
+import solidPlugin from "vite-plugin-solid";
+import sitemap from "vite-sitemap";
 import buildconfing from "./vite-config/build";
 import "./vite-config/global";
-import AddPlugins from "./vite-config/plugins";
+import manifest from "./vite-config/manifest";
+import pwa from "./vite-config/pwa";
 
 
 export default defineConfig({
-  plugins: AddPlugins,
+  plugins: [
+    solidPlugin(),
+    sitemap({
+      baseURL: process.env.VITE_LIVE_URL,
+      urls: [
+        "license",
+        "privacy-policy",
+        'term-and-conditions',
+        'help-center'
+      ],
+    }),
+    pwa({ manifest: manifest })
+  ],
   server: {
     port: Number(process.env.VITE_PORT) || 31000,
   },

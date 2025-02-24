e1a5f67324ebc3c5b482e707adc2775afb4a9994
Saeed <78552486+appsaeed@users.noreply.github.com>
2025-02-24 13:34:40 +0600

    Update HeroSection.tsx

--- src/views/sections/HeroSection.tsx
+++ src/views/sections/HeroSection.tsx
@@ -80,7 +80,7 @@ export default function ({ class: className, ...props }: HtmlAttr) {
         </Animate.h2>
 
         <Animate.a
-          href={'https://raw.githubusercontent.com/appsaeed/assets/refs/heads/me/resume/Laravel-developer-saeed.pdf'}
+          href={'https://raw.githubusercontent.com/appsaeed/assets/refs/heads/me/resume/Saeed_Hosan_Laravel_Developer.pdf'}
           motion="slideInUp"
           type="button"
           class="text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-10 py-2 text-center mr-2 mb-2"
